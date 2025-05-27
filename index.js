228b9f96916f32d6aacb3a9a7b3dbe4c998fb66b
AndreyGOIT <ae.borga@gmail.com>
2025-05-27 11:03:59 +0300

    Handle an Action in the Store

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const reducer = (state = 5) => {
-  return state;
+const defaultState = {
+  login: false,
 };
 
-// Redux methods are available from a Redux object
-// For example: Redux.createStore()
-// Define the store here:
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  if (action.type === "LOGIN") {
+    return { login: true };
+  } else {
+    return state;
+  }
+  // switch (action.type) {
+  //   case "LOGIN":
+  //     return { login: true };
+  //   default:
+  //     return state;
+  // }
+  // Change code above this line
+};
+
 const store = Redux.createStore(reducer);
 
-const currentState = store.getState();
-
-const action = {
-  type: "LOGIN",
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
 };
-
-function actionCreator() {
-  return action;
-}
-store.dispatch(actionCreator());
