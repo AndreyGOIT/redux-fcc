2b36e9eaf4fa27f6e1ed59c682c127c918af2804
AndreyGOIT <ae.borga@gmail.com>
2025-05-28 13:11:32 +0300

    Extract State Logic to Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({ reducer: {} });
+const ADD = "ADD";
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message,
+  };
+};
+console.log(addMessage("Hello"));
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+};
+const store = configureStore(messageReducer);
+
 export default store;
