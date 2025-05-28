2046424f0532c5644711ed769d77136ebc6dfb71
AndreyGOIT <ae.borga@gmail.com>
2025-05-28 11:07:05 +0300

    Getting Started with React Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // import { useState } from 'react'
 import "./App.css";
+import DisplayMessages from "./components/component";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  return <>Hello</>;
+  return (
+    <div className="App">
+      <DisplayMessages />
+    </div>
+  );
 }
 
 export default App;
